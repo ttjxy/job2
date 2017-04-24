dbc80bf3b8692e0ff79c0ee08c3d5d0d84ffde61
xy <ttjxy@gmail.com>
2017-04-25 01:00:04 +0800

    implement group CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170424155430) do
+ActiveRecord::Schema.define(version: 20170424163327) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
